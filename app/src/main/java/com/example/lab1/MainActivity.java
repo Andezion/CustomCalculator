f5bcb9fdaf351cfa310691738ada4875902dad6e
Andezion <245122@edu.p.lodz.pl>
2025-03-20 22:38:14 +0100

    добавил ввод правильного текста

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -10,14 +10,53 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity
 {
     private TextView textView;
-    private Button button;
+    private StringBuilder inputText = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Подключение разметки
+        setContentView(R.layout.activity_main);
 
-        
+        textView = findViewById(R.id.textView2);
+
+        View.OnClickListener buttonClick = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Button button = (Button) v;
+                inputText.append(button.getText().toString());
+                updateTextView();
+            }
+        };
+
+        int[] buttonID = {R.id.button13, R.id.button14, R.id.button15, R.id.button9, R.id.button10,
+                R.id.button11, R.id.button5, R.id.button6, R.id.button7, R.id.button18};
+
+        for(int id : buttonID)
+        {
+            Button button = findViewById(id);
+            button.setOnClickListener(buttonClick);
+        }
+
+        Button deleteButton = findViewById(R.id.button1);
+        deleteButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(inputText.length() > 0)
+                {
+                    inputText.deleteCharAt(inputText.length() - 1);
+                    updateTextView();
+                }
+            }
+        });
+    }
+
+    private void updateTextView()
+    {
+        textView.setText(inputText.toString());
     }
 }
